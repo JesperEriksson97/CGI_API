6274af4b15bb63d1ac3646ae685cb338e924174a
je223gs <je223gs@student.lnu.se>
2021-02-20 00:15:12 +0100

    adding updates to post request

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-  
+const User = require('../model/User')
 const userController = {}
 
 userController.getExampleUser = (req, res) => {
@@ -18,7 +18,26 @@ userController.getSpecificUser = (req, res) => {
 
 userController.saveUser = (req, res) => {
     console.log(req.body)
+
+    const email = req.body.email
+    const password = req.body.password
+    const username = req.body.username
+    const fullname = req.body.fullname
+
+    const newUser = new User({
+        email: email,
+        username: username,
+        fullname: fullname,
+        password: password
+    })
+
+    User.save(() => {
+        res.send({ "msg": "Success" })
+    })
+
+
 }
+
 
 userController.updateUser = (req, res) => {
     console.log(req.body)
